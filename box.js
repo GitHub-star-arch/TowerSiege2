c6b038b1beb68c1118710f1e5160f82a874603b1
GitHub-star-arch <nithinraggov@gmail.com>
2020-05-25 16:14:46 -0400

    huh

--- box.js
+++ box.js
@@ -4,12 +4,19 @@ class Box {
       this.body = Bodies.rectangle(x,y,width,height);
       this.width = width;
       this.height = height;
+      this.Visiblity = 255;
       World.add(world, this.body);
     }
     display(){
       var pos =this.body.position;
       rectMode(CENTER);
-      fill(200,43,123);
+      fill("gray");
       rect(pos.x, pos.y, this.width, this.height);
+      if(this.body.speed > 9){
+         push();
+         this.Visiblity = this.Visiblity - 50;
+         tint(255,this.Visiblity);
+         pop();
+      }
     }
   };
--- sketch.js
+++ sketch.js
@@ -5,32 +5,27 @@ const Constraint= Matter.Constraint;
 var posx;
 var posy;
 var g1,g2,ball,string;
-var b1,b2,b3,b4,b5,b6,b7,b8,b9,b10;
+var b1,b2,b3,b4,b5,b6;
 
 function setup(){
     var canvas = createCanvas(800,600);
     engine = Engine.create();
     world = engine.world;
-    b1= new Box(600,190,20,20);
-    b2= new Box(620,190,20,20);
-    b3= new Box(640,190,20,20);
-    b4= new Box(610,170,20,20);
-    b5= new Box(630,170,20,20);
-    b6= new Box(600,290,20,20);
-    b7= new Box(620,290,20,20);
-    b8= new Box(640,290,20,20);
-    b9= new Box(610,270,20,20);
-    b10= new Box(630,270,20,20);
+    b1 = new Box(300,400,40,40);
+    b2 = new Box(340,400,40,40);
+    b3 = new Box(380,400,40,40);
+    b4 = new Box(320,360,40,40);
+    b5 = new Box(360,360,40,40);
+    b6 = new Box(340,320,40,40);
     /*var options={
         isStatic:true
     }*/
-    ball= new Ball(200,400,20,20)
+    ball= new Ball(200,400,40,40)
     /*posx = mouseX;
     posy = mouseY;
     var ball= ellipse(posx,posy,20,20);*/
-    string = new String(ball.body,{x: 200,y: 400});
-    g1= new Ground(650,200,200,10);
-    g2= new Ground(650,400,200,10);
+    string = new String(ball.body,{x: 150,y: 400});
+    g1= new Ground(350,550,200,10);
 }   
 
 function draw(){
@@ -41,7 +36,6 @@ function draw(){
     ellipseMode(CENTER);
    ellipse(posx,posy,20,20);*/
    g1.display();
-   g2.display();
    string.display();
    ball.display();
    b1.display();
@@ -50,10 +44,6 @@ function draw(){
    b4.display();
    b5.display();
    b6.display();
-   b7.display();
-   b8.display();
-   b9.display();
-   b10.display();
 }
 
 function mouseDragged () {
